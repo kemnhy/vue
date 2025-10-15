0719a5fe2489623eaefe078771480278d812450a
keemnhy <keemnhy@gmail.com>
2025-10-15 13:19:16 +0900

    ~~

---
+++ computed/src/components/Computed_ex1.vue
@@ -0,0 +1,39 @@
+x
+<template>
+  <div>
+    <h2>장바구니</h2>
+    <p>상품가격 : {{ formatNumber(price) }}</p>
+    <p>할인 (20%) : {{formatNumber(discount)}}</p>
+    <p>세금 (10%) : {{formatNumber(tax)}}</p>
+    <hr />
+    <p>
+      <strong>최종 결제금액 :{{formatNumber(total)}}원</strong>
+    </p>
+  </div>
+</template>
+<script setup>
+import { computed, ref } from "vue";
+// 천단위 함수
+const formatNumber = (num) => {
+  return num.toLocaleString();
+};
+// 원래가격
+const price = ref(10000);
+// 할인 금액 (20%)
+const discount = computed(() => {
+ return price.value * 0.2;
+});
+// 세금(10%)
+const tax = computed(()=>{
+    // return (원래가격 - 할인 금액) * 0.1
+    return (price.value - discount.value) * 0.1;
+})
+// 최종금액
+// 원래금액 - 할인금액 + 세금
+const total = computed(()=>{
+return price.value - discount.value + tax.value
+})
+
+</script>
+
+<style scoped></style>
---
+++ computed/src/components/V_model.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input  v-model="message" @input=" updateMessage" type="text" placeholder="메세지 입력" />
+    <p>입력한 내용 :{{ message }}</p>
+    <input v-model="discount" type="text" placeholder="할인금액">
+    <p>할인 금액 : {{  discount }}</p>
+  </div>
+</template>
+<script setup>
+import { nextTick, ref } from 'vue';
+const message = ref("");
+const discount = ref("")
+// 한글이슈에 대한 코드 
+const updateMessage = ((event)=>{
+    nextTick(()=>{
+message.value = event.target.value;
+    });
+});
+
+</script>
+<style scoped></style>
